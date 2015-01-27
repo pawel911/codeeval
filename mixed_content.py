cf3e51e19c4f8842720bf8840dc0ea82a609e0d6
paweldymowski <pawel911@gmail.com>
2015-01-27 15:13:01 +0100

    MIXED CONTENT codeeval
    Challenge description:
    
    You have a string of words and digits divided by comma. Write a program which separates words with digits. You shouldn't change the order elements.
    
    INPUT SAMPLE:
    
    Your program should accept as its first argument a path to a filename. Input example is the following
    
    8,33,21,0,16,50,37,0,melon,7,apricot,peach,pineapple,17,21
    24,13,14,43,41
    OUTPUT SAMPLE:
    
    melon,apricot,peach,pineapple|8,33,21,0,16,50,37,0,7,17,21
    24,13,14,43,41
    As you cas see you need to output the same input string if it has words only or digits only.

---
+++ mixed_content.py
@@ -0,0 +1,27 @@
+import sys
+
+test_cases = open(sys.argv[1], 'r')
+
+for line in test_cases:
+    
+    line = line.rstrip("\n").split(',')
+    
+    numbers = []
+    strings =[]
+    
+    for i in range(len(line)):
+        if line[i][0].isdigit :
+           numbers.append(line[i])
+           
+        else:
+           strings.append(line[i])
+           
+    if len(strings) > 0 and len(numbers) > 0:
+        output = ",".join(strings) + "|" + ",".join(numbers)
+    elif len(strings) == 0: output = ",".join(numbers)
+    elif len(numbers) == 0: output = ",".join(strings)
+    else: output = ""  
+          
+    print output
+      
+test_cases.close()
